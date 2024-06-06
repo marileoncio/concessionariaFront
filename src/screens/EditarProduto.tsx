10d07ca1f1e1758e9c3421abeae3312fa9c24164
marileoncio <mariana.timoteo19@yahoo.com>
2024-06-06 09:28:17 -0300

    axios

--- src/screens/EditarProduto.tsx
+++ src/screens/EditarProduto.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Footer from "../components/Footer";
 import Head from "../components/Head";
+import axios from "axios";
 
 interface Carros {
     id: number;
@@ -18,6 +19,7 @@ interface Carros {
 }
 
 const EditarProduto: React.FC = () => {
+    const [id, setId] = useState<string>('');
     const [modelo, setModelo] = useState<string>('');
     const [ano, setAno] = useState<string>('');
     const [marca, setMarca] = useState<string>('');
@@ -33,7 +35,7 @@ const EditarProduto: React.FC = () => {
     useEffect(() => {
 
      const {item} = route.params;
-
+         setId(item.id)
         setModelo(item.modelo);
         setAno(item.ano);
         setMarca(item.marca);
@@ -43,74 +45,80 @@ const EditarProduto: React.FC = () => {
         setDescricao(item.descricao);
         setPreco(item.valor);
     },[])
+
+    const atualizar = () => {
+        const dadosDoCarro = {
+            id: id,
+            modelo: modelo,
+            ano: ano,
+            marca: marca,
+            cor: cor,
+            peso: peso,
+            potencia: potencia,
+            descricao: descricao,
+            preco: preco,
+        };
+        axios.put("http://10.137.11.229:8000/api/carros/update", dadosDoCarro, {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        }).then(response => {
+            console.log(response.data);
+            navigation.goBack(); // Redireciona para a tela anterior após a atualização bem-sucedida
+        }).catch(error => {
+            console.error(error);
+        });
+    }
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor="red" barStyle="light-content" />
             <Head/>
 
-            <View style={styles.form}>
-                <TextInput
-                    style={styles.input}
-                    value={modelo}
-                    onChangeText={setModelo}
-                />
+            <View style={styles.container}>
+            <TextInput value={modelo}
+                 onChangeText={setModelo}
+                  style={styles.input} />
 
-                <TextInput
-                    style={styles.input}
-                    value={ano}
-                    onChangeText={setAno}
+                <TextInput value={ano}
+                 onChangeText={setAno}
+                  keyboardType="numeric"
+                   style={styles.input} />
 
-                />
+                <TextInput value={marca}
+                 onChangeText={setMarca}
+                  style={styles.input} />
 
-                <TextInput
-                    style={styles.input}
-                    value={marca}
-                    onChangeText={setMarca}
+                <TextInput value={cor}
+                 onChangeText={setCor}
+                  style={styles.input} />
 
-                />
+                <TextInput value={peso}
+                 onChangeText={setPeso}
+                  style={styles.input} />
 
-                <TextInput
-                    style={styles.input}
-                    value={cor}
-                    onChangeText={setCor}
+                <TextInput value={potencia}
+                 onChangeText={setPotencia}
+                  style={styles.input} />
 
-                />
+                <TextInput value={descricao}
+                 onChangeText={setDescricao}
+                  multiline
+                   style={styles.input} />
 
-                <TextInput
-                    style={styles.input}
-                    value={peso}
-                    onChangeText={setPeso}
+                <TextInput value={preco}
+                 onChangeText={setPreco}
+                  keyboardType="numeric"
+                   style={styles.input} />
 
-                />
-
-                <TextInput
-                    style={styles.input}
-                    value={potencia}
-                    onChangeText={setPotencia}
-
-                />
-
-                <TextInput
-                    style={styles.input}
-                    value={descricao}
-                    onChangeText={setDescricao}
-
-                />
-
-                <TextInput
-                    style={styles.input}
-                    value={preco}
-                    onChangeText={setPreco}
-
-                />
-
+                <TouchableOpacity onPress={atualizar} style={styles.button}>
+                    <Text style={styles.buttonText}>Editar</Text>
+                </TouchableOpacity>
                 <TouchableOpacity style={styles.button}
                     onPress={() => navigation.goBack()}>
                     <Text style={styles.buttonText}>Voltar</Text>
                 </TouchableOpacity>
 
-                <View style={styles.menuList}></View>
-                
             </View>
             <Footer />
         </View>
@@ -119,68 +127,31 @@ const EditarProduto: React.FC = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-    },
-    header: {
-        backgroundColor: "red",
-        paddingVertical: 10,
-        alignItems: 'center'
-    },
-    headerText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white'
-
-    },
-    form: {
-        padding: 10,
-        backgroundColor: "#f0f0f0",
-        marginBottom: 10
+        flex: 1
     },
     input: {
+        margin: 10,
         height: 40,
-        borderColor: 'gray',
+        borderColor: 'black',
         borderWidth: 1,
-        marginBottom: 10,
+        marginBottom: 5,
         paddingHorizontal: 10,
         borderRadius: 10
     },
-    imageButton: {
-        backgroundColor: 'red',
+    button: {
+        backgroundColor: '#3a415a',
         padding: 10,
         borderRadius: 5,
         alignItems: 'center',
-        marginBottom: 10
-    },
-    imageButtonText: {
-        color: 'white',
-        fontWeight: 'bold'
-    },
-    imagemSelecionada: {
-        width: 200,
-        height: 200,
-        resizeMode: 'cover',
-        borderRadius: 5,
         marginBottom: 10,
-    },
-    alinhamentoImagemSelecionada: {
-        alignItems: 'center'
-    },
-    button: {
-        backgroundColor: 'red',
-        padding: 10,
-        borderRadius: 5,
-        alignItems: 'center'
+        borderColor: 'white'
     },
     buttonText: {
         color: 'white',
         fontWeight: 'bold'
-    },
-    menuList: {
-        flexGrow: 1
-    },
+    }
+});
 
 
-})
 
 export default EditarProduto;
